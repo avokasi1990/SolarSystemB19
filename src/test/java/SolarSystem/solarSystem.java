5312ecdb13e3895d2954a0c1ab95d0eb94d7cc9a
avokasi1990 <zarinaisakova30@gmail.com>
2024-06-08 22:58:15 -0400

    mvp solar system basics

---
+++ src/test/java/SolarSystem/solarSystem.java
@@ -0,0 +1,21 @@
+package SolarSystem;
+
+public class solarSystem {
+    public static void main(String[] args) {
+        solarSystemBase solar = new solarSystemBase();
+        solar.sun ="1";
+        solar.planet ="2";
+        solar.star ="2";
+        System.out.println("The solar system is ready");
+
+        /// .....
+
+        featureSun xros = new featureSun();
+        xros.color="pink";
+        xros.temp="24324kv";
+        xros.size="0km";
+    }
+}
+
+
+
